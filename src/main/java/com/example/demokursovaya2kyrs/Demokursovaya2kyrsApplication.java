baaa47b37d227441f186f3e3cb094cd80fab6fda
Илон <vlasovvdm@gmail.com>
2024-04-10 18:00:19 +0400

    !

---
+++ src/main/java/com/example/demokursovaya2kyrs/Demokursovaya2kyrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokursovaya2kyrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demokursovaya2kyrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demokursovaya2kyrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demokursovaya2kyrs/Demokursovaya2kyrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demokursovaya2kyrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demokursovaya2kyrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
